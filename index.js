c5fb3cf9983dc881eba13f3a5c83600da74ec0ca
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-10-31 13:04:53 -0400

    final product

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result;
+  if (someNumber <= 400) {
+  return 'This one is on me!';
+} else if (someNumber <= 2000) {
+  return 'That will be twenty bucks.';
+} else if (someNumber > 2000 && someNumber < 2500) {
+  return 'I will gladly take your thirty bucks.';
+} else if (someNumber > 2500) {
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  let cityResult;
+  if (someCity === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someCharm){
+  let charmResult;
+  switch (someCharm) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
